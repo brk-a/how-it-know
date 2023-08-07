1e758ea13fae480617547d973c273dc0fc180675
brk-a <fnjakai@outlook.com>
2023-08-07 04:29:53 +0300

    parser and interpreter

--- 0x01-how_it_know/app/shell.py
+++ 0x01-how_it_know/app/shell.py
@@ -1,7 +1,13 @@
 from lexer import Lexer
+from parser import Parser
+from interpreter import Interpreter
 
 while True:
     text = input("FrankenScript: ~/$:  ")
     tokeniser = Lexer(text)
     tokens = tokeniser.tokenise()
-    print(tokens)
+    parser = Parser(tokens)
+    tree = parser.parse()
+    interpreter = Interpreter(tree)
+    result = interpreter.interpret()
+    print(result)
